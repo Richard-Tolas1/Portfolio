9a7f998106116e9fa581788f38ad423c9daa3fd1
Ijitola Richard <98714695+Richard-Tolas1@users.noreply.github.com>
2024-05-04 16:22:15 +0100

    Home Page Update

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,74 +1,60 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Loader from 'react-loaders'
-import LogoTitle from '../../assets/images/logo-r2.png';
-import AnimatedLetters from '../AnimatedLetters';
-import Logo from './Logo'
-import './index.scss';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Loader from "react-loaders";
+import LogoTitle from "../../assets/images/logo-r2.png";
+import AnimatedLetters from "../AnimatedLetters";
+import Logo from "./Logo";
+import "./index.scss";
 
 const Home = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
-  const nameArray = ['i', 'c', 'h', 'a', 'r', 'd']
-  const jobArray = [
-    'W',
-    'e',
-    'b',
-    ' ',
-    'D',
-    'e',
-    'v',
-    'e',
-    'l',
-    'o',
-    'p',
-    'e',
-    'r',
-    '.',
-  ]
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray = ["i", "c", "h", "a", "r", "d"];
+  const jobArray = ["I", "j", "i", "t", "o", "l", "a", "."];
 
   useEffect(() => {
-     setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 4000)
-  }, [])
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 4000);
+  }, []);
 
-	return (
+  return (
     <>
-		<div className="container home-page">
-			<div className="text-zone">
-				<h1>
-				<span className={letterClass}>H</span>
-				<span className={`${letterClass} _12`}>i,</span>		
-				<br />  
-				<span className={`${letterClass} _13`}>I</span>
-				<span className={`${letterClass} _14`}>'m</span>
-        <img
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img
               src={LogoTitle}
               alt="JavaScript Developer Name, Web Developer Name"
-        />
-				<AnimatedLetters
+            />
+            <AnimatedLetters
               letterClass={letterClass}
               strArray={nameArray}
               idx={15}
             />
-				<br />
-				<AnimatedLetters
+            <br />
+            <AnimatedLetters
               letterClass={letterClass}
               strArray={jobArray}
               idx={22}
             />
-				</h1>
-				<h2>Web Developer / Mobile Developer</h2>
-				<Link to="/contact" className='flat-button'>
-          CONTACT ME
+            <span className={`${letterClass} _15`}>a</span>
+          </h1>
+          <h2>Software Developer</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
           </Link>
-			</div>
-      <Logo />
-		</div>
+        </div>
+        <Logo />
+      </div>
 
-    <Loader type="pacman" />
+      <Loader type="pacman" />
     </>
-	)
-}
+  );
+};
 
-export default Home
+export default Home;
